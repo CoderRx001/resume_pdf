2bd4bf11270ea1965a05effc3a7e723fb3527f15
natail <nataleanfuso@gmail.com>
2017-07-02 11:46:46 -0500

    created show action in students_controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,44 @@
 class StudentsController < ApplicationController
+    def index
+        @students = "sample data"
+    end
+
+    def show
+        @student = {
+                    first_name: "",
+                    last_name: '',
+                    email: '',
+                    phone_number: '',
+                    bio: '',
+                    linkedin_url: '',
+                    twitter_url: '',
+                    blog_url: '',
+                    resume_url: '',
+                    github_url: '',
+                    photo: '',
+                    experience: [{
+                                start_date: '',
+                                end_date: '',
+                                job_title: '',
+                                company_name: '',
+                                details: ''
+                                }],
+                    education: [{
+                                start_date: '',
+                                end_date: '',
+                                degree: '',
+                                university_name: '',
+                                details: ''
+                                }],
+                    skills: [{
+                                skill_name: ''
+                            }],
+                    capstone: [{
+                                name: '',
+                                description: '',
+                                url: '',
+                                screenshot: ''
+                        }]
+                    },
+    end
 end
